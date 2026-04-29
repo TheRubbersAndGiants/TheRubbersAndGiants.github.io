d7119220c1e85f070b934ccfbda4a524afe15ecf
TheRubbersAndGiants <randomnerd910@gmail.com>
2026-04-29 20:47:33 +1000

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,11 @@
-const btn = document.getElementById('menuBtn');
-const menu = document.getElementById('navMenu');
-
-btn.addEventListener('click', () => {
-  menu.classList.toggle('show');
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('menuBtn');
+  const menu = document.getElementById('navMenu');
+  if (btn && menu) {
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
+  } else {
+    console.error("Menu or Button ID not found. Check your HTML IDs!");
+  }
 });
