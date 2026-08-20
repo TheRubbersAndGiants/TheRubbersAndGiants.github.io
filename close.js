70aeeee3cad1292f95fccffde14921d3d21fdf07
TheRubbersAndGiants <randomnerd910@gmail.com>
2026-08-20 22:32:48 +1000

    Update close.js

--- close.js
+++ close.js
@@ -1,9 +1,9 @@
-document.querySelectorAll('h1, h2, th1-f').forEach(header => {
+document.querySelectorAll('h1, h2, th1-f, footer').forEach(header => {
     header.style.cursor = "pointer";
     header.addEventListener('click', () => {
         const isH1 = header.tagName === 'H1';
         let next = header.nextElementSibling;
-        const stopTags = isH1 ? ['H1'] : ['H1', 'H2', 'TH1-F'];
+        const stopTags = isH1 ? ['H1'] : ['H1', 'H2', 'TH1-F', 'FOOTER'];
         const isOpening = !next || window.getComputedStyle(next).display === "none";
         while (next && !stopTags.includes(next.tagName)) {
             if (isOpening) {
